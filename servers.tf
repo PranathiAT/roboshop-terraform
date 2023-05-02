60325046654119235619310e5a1536e26f82ddb3
Pranathi Alapati <tpranathi04@gmail.com>
2023-05-02 12:15:38 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,12 +1,9 @@
 data "aws_ami" "centos" {
-  owners        = ["973714476881"]
-  most_recent   = true
+  most_recent = true
+  owners = ["973714476881"]
   name_regex    = "centos-8-DevOps-Practice"
 }
 
-output "ami" {
-  value = data.aws_ami.centos.image_id
-}
 
 
 resource "aws_instance" "frontend" {
