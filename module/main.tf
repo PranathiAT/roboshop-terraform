aa90b31cc86753327a8c03a3832d8f94c18b4b23
Pranathi Alapati <tpranathi04@gmail.com>
2023-05-15 19:40:45 +0530

    create IAM policy in terraform

--- module/main.tf
+++ module/main.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "instance" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow_all.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
-    Name = var.component_name
+    Name = local.name
   }
 }
 resource "null_resource" "provisioner" {
@@ -90,4 +91,9 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
       ]
     }
   )
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component_name}-${var.env}-role"
+  role = aws_iam_role.role.name
 }
