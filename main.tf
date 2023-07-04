ccb846632f1b33cdea7918cecf5848fd2035f6a1
Pranathi Alapati <tpranathi04@gmail.com>
2023-07-04 10:23:28 +0530

    eks module

--- main.tf
+++ main.tf
@@ -12,118 +12,131 @@ module "vpc" {
 }
 
 
-module "docdb"{
-  source= "git::https://github.com/PranathiAT/tf-module-docdb.git"
+#module "docdb"{
+#  source= "git::https://github.com/PranathiAT/tf-module-docdb.git"
+#
+#  for_each = var.docdb
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  kms_arn = var.kms_arn
+#  engine_version = each.value["engine_version"]
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#
+#
+#}
+#
+#
+#module "rds"{
+#  source= "git::https://github.com/PranathiAT/tf-module-rds.git"
+#
+#  for_each = var.rds
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  kms_arn = var.kms_arn
+#  engine_version = each.value["engine_version"]
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#
+#}
+#
+#
+#module "elasticache"{
+#  source= "git::https://github.com/PranathiAT/tf-module-elasticache.git"
+#
+#  for_each = var.elasticache
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  kms_arn = var.kms_arn
+#  engine_version = each.value["engine_version"]
+#  replicas_per_node_group = each.value["replicas_per_node_group"]
+#  num_node_groups = each.value["num_node_groups"]
+#  node_type = each.value["node_type"]
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#
+#}
+#
+#module "rabbitmq"{
+#  source= "git::https://github.com/PranathiAT/tf-module-amazon-mq.git"
+#
+#  for_each = var.rabbitmq
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  kms_arn = var.kms_arn
+#  instance_type = each.value["instance_type"]
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#  bastion_cidr = var.bastion_cidr
+#  domain_id = var.domain_id
+#}
+#
+#module "alb"{
+#  source= "git::https://github.com/PranathiAT/tf-module-alb.git"
+#
+#  for_each = var.alb
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_alb_cidr = each.value["name"]== "public" ? ["0.0.0.0/0"] :concat(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null),lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_cidrs", null))
+#  internal = each.value["internal"]
+#  name = each.value["name"]
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#
+#}
 
-  for_each = var.docdb
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  kms_arn = var.kms_arn
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
+#module "app" {
+#  depends_on = [module.vpc,module.alb,module.docdb,module.rabbitmq,module.elasticache,module.rds]
+#  source= "git::https://github.com/PranathiAT/tf-module-app.git"
+#
+#  for_each = var.app
+#  instance_type = each.value["instance_type"]
+#  name = each.value["name"]
+#  desired_capacity = each.value["desired_capacity"]
+#  max_size = each.value["max_size"]
+#  min_size = each.value["min_size"]
+#  app_port = each.value["app_port"]
+#  listener_priority = each.value["listener_priority"]
+#  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+#  parameters = each.value["parameters"]
+#
+#  env = var.env
+#  bastion_cidr = var.bastion_cidr
+#  tags = merge(local.tags,{Monitor = "true"})
+#  domain_name = var.domain_name
+#  domain_id = var.domain_id
+#  kms_arn = var.kms_arn
+#  monitor_cidr = var.monitor_cidr
+#
+#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  vpc_id =lookup(lookup(module.vpc, "main" , null ), "vpc_id" , null)
+#  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+#  listener_arn = lookup(lookup(module.alb, each.value["lb_type"] , null ), "listener_arn" , null)
+#  lb_dns_name = lookup(lookup(module.alb, each.value["lb_type"] , null ), "dns_name" , null)
+#}
 
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-
-
+module "eks" {
+  source             ="git::https://github.com/PranathiAT/tf-module-eks.git"
+  ENV                = var.env
+  eks_version        = 1.27
+  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
+  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+  kms_arn            = var.kms_arn
 }
 
-
-module "rds"{
-  source= "git::https://github.com/PranathiAT/tf-module-rds.git"
-
-  for_each = var.rds
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  kms_arn = var.kms_arn
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-
-}
-
-
-module "elasticache"{
-  source= "git::https://github.com/PranathiAT/tf-module-elasticache.git"
-
-  for_each = var.elasticache
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  kms_arn = var.kms_arn
-  engine_version = each.value["engine_version"]
-  replicas_per_node_group = each.value["replicas_per_node_group"]
-  num_node_groups = each.value["num_node_groups"]
-  node_type = each.value["node_type"]
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-
-}
-
-module "rabbitmq"{
-  source= "git::https://github.com/PranathiAT/tf-module-amazon-mq.git"
-
-  for_each = var.rabbitmq
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  kms_arn = var.kms_arn
-  instance_type = each.value["instance_type"]
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-  bastion_cidr = var.bastion_cidr
-  domain_id = var.domain_id
-}
-
-module "alb"{
-  source= "git::https://github.com/PranathiAT/tf-module-alb.git"
-
-  for_each = var.alb
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_alb_cidr = each.value["name"]== "public" ? ["0.0.0.0/0"] :concat(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null),lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_cidrs", null))
-  internal = each.value["internal"]
-  name = each.value["name"]
-  tags = local.tags
-  env = var.env
-  vpc_id = local.vpc_id
-
-}
-
-module "app" {
-  depends_on = [module.vpc,module.alb,module.docdb,module.rabbitmq,module.elasticache,module.rds]
-  source= "git::https://github.com/PranathiAT/tf-module-app.git"
-
-  for_each = var.app
-  instance_type = each.value["instance_type"]
-  name = each.value["name"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size = each.value["max_size"]
-  min_size = each.value["min_size"]
-  app_port = each.value["app_port"]
-  listener_priority = each.value["listener_priority"]
-  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
-  parameters = each.value["parameters"]
-
-  env = var.env
-  bastion_cidr = var.bastion_cidr
-  tags = merge(local.tags,{Monitor = "true"})
-  domain_name = var.domain_name
-  domain_id = var.domain_id
-  kms_arn = var.kms_arn
-  monitor_cidr = var.monitor_cidr
-
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  vpc_id =lookup(lookup(module.vpc, "main" , null ), "vpc_id" , null)
-  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-  listener_arn = lookup(lookup(module.alb, each.value["lb_type"] , null ), "listener_arn" , null)
-  lb_dns_name = lookup(lookup(module.alb, each.value["lb_type"] , null ), "dns_name" , null)
-}
 
 #### Load Runner
 data "aws_ami" "ami" {
